3463a46a76c6fba7d18be1572229cf74f0fe2667
ubet <ubetapatis90@gmail.com>
2020-04-02 15:29:16 +0700

    Updates

---
+++ precache-manifest.0db9e0867ebf516633bfd4fff863b455.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "001c346d717ffb308c8c7a272a89f59b",
+    "url": "/Todo-app/index.html"
+  },
+  {
+    "revision": "d51d04cf5f05577d6dc7",
+    "url": "/Todo-app/static/css/main.d0c3b28a.chunk.css"
+  },
+  {
+    "revision": "d2f790052f801c14c2bf",
+    "url": "/Todo-app/static/js/2.9814598f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-app/static/js/2.9814598f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d51d04cf5f05577d6dc7",
+    "url": "/Todo-app/static/js/main.aff8f4a1.chunk.js"
+  },
+  {
+    "revision": "66d6147545c967366f86",
+    "url": "/Todo-app/static/js/runtime-main.ecda6997.js"
+  },
+  {
+    "revision": "78ee9c8a6a9f322010a59e932bc83920",
+    "url": "/Todo-app/static/media/background.78ee9c8a.png"
+  }
+]);
